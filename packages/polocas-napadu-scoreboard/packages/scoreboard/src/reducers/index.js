b62e2e6791e7cdf86f9972155dfba493625c46b0
Pavel Žák <pavel@berrycloud.cz>
2018-11-01 09:55:54 +0100

    Fix spectator reducer source

--- packages/polocas-napadu-scoreboard/packages/scoreboard/src/reducers/index.js
+++ packages/polocas-napadu-scoreboard/packages/scoreboard/src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 
 import board from '../board/reducers';
-import spectator from '../board/reducers';
+import spectator from '../spectator/reducers';
 
 export default combineReducers({
   board,
