962e78c852cad4d4873dc38ceab850546d481a61
Pavel Žák <pavel@berrycloud.cz>
2018-11-05 19:02:26 +0100

    Don't trigger monitor sagas in spectator window

--- packages/polocas-napadu-scoreboard/packages/scoreboard/src/editor/sagas/index.js
+++ packages/polocas-napadu-scoreboard/packages/scoreboard/src/editor/sagas/index.js
@@ -3,6 +3,7 @@ import { put, select, takeEvery } from 'redux-saga/effects';
 import { spectatorWindow } from '../../spectator/actions';
 import { team } from '../../teams/actions';
 import { teamEdit } from '../actions';
+import { monitorOnly } from '../../spectator/sagas';
 
 function* syncWindow() {
   const state = yield select(state => state);
@@ -15,13 +16,13 @@ function* closeForm(action) {
   yield put(teamEdit.hide(null, action.meta));
 }
 
-function* handleSpectatorJoin() {
+const handleSpectatorJoin = monitorOnly(function* () {
   yield takeEvery(spectatorWindow.JOIN, syncWindow);
-}
+});
 
-function* handleTeamEditSubmit() {
+const handleTeamEditSubmit = monitorOnly(function* () {
   yield takeEvery(team.DATA_CHANGE, closeForm);
-}
+});
 
 export default [
   handleSpectatorJoin,
