bed5b90c450a894d500bdfb1b21e319e1eb47ab4
just-paja <just.paja@gmail.com>
2019-01-27 16:00:15 +0100

    Make basic test run again

--- packages/polocas-napadu-scoreboard/packages/scoreboard/src/App.test.js
+++ packages/polocas-napadu-scoreboard/packages/scoreboard/src/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { SchemaLink } from 'apollo-link-schema';
+import { makeExecutableSchema } from 'graphql-tools';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('app', () => {
+  let client;
+
+  beforeEach(() => {
+    const schema = makeExecutableSchema({
+      typeDefs: 'type Show { id: Int! }',
+    });
+    client = new ApolloClient({
+      cache: new InMemoryCache(),
+      link: new SchemaLink({ schema }),
+    });
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App client={client} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
