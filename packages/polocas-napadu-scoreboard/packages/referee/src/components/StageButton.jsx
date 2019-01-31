f9681856bda6c5f3aca10cedf60082b890de2de3
just-paja <just.paja@gmail.com>
2019-02-01 00:07:44 +0100

    WIP: Stage button

--- packages/polocas-napadu-scoreboard/packages/referee/src/components/StageButton.jsx
+++ packages/polocas-napadu-scoreboard/packages/referee/src/components/StageButton.jsx
@@ -1,4 +1,5 @@
 import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import PropTypes from 'prop-types';
 import React from 'react';
 
@@ -9,32 +10,49 @@ import { STAGES } from 'core/constants';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
+  buttonProgress: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginTop: -12,
+    marginLeft: -12,
+  },
+  wrapper: {
+    display: 'inline-flex',
+    position: 'relative',
+  },
 });
 
 const CHANGE_STAGE = gql`
   mutation ChangeStage($matchId: Int!, $stage: String!) {
-    changeStage(id: $matchId, stage: $stage) {
-      id,
+    changeMatchStage(matchId: $matchId, stage: $stage) {
+      ok,
     }
   }
 `;
 
 class StageButton extends React.Component {
   render() {
-    const { stage } = this.props;
+    const { classes, stage } = this.props;
     return (
       <Mutation mutation={CHANGE_STAGE}>
         {(changeStage, { error, loading }) => {
           return (
-            <Button
-              disabled={loading}
-              onClick={() => changeStage({
-                variables: {
-                  matchId: this.context.match.id,
-                  stage,
-                },
-              })}
-            >{stage}</Button>
+            <div className={classes.wrapper}>
+              <Button
+                disabled={loading}
+                onClick={() => changeStage({
+                  variables: {
+                    matchId: this.context.match.id,
+                    stage,
+                  },
+                })}
+              >{stage}</Button>
+              {loading && <CircularProgress
+                size={24}
+                className={classes.buttonProgress}
+              />}
+            </div>
           );
         }}
       </Mutation>
