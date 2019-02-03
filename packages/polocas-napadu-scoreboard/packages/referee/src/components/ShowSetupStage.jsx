6e93894368fcf8bfdecf0c3e626eaddb4e2cc4fb
just-paja <just.paja@gmail.com>
2019-02-03 19:15:18 +0100

    Add info to match setup stage

--- packages/polocas-napadu-scoreboard/packages/referee/src/components/ShowSetupStage.jsx
+++ packages/polocas-napadu-scoreboard/packages/referee/src/components/ShowSetupStage.jsx
@@ -6,6 +6,7 @@ import { TEAM_SIDE_LEFT, TEAM_SIDE_RIGHT } from 'core/constants';
 
 import BoardLayout from './BoardLayout';
 import ControlsLayout from './ControlsLayout';
+import MainControls from './MainControls';
 import Team from './Team';
 
 const styles = {
@@ -22,6 +23,13 @@ const ShowSetupStage = ({ classes }) => (
       <Team side={TEAM_SIDE_LEFT} />
       <Team side={TEAM_SIDE_RIGHT} />
     </BoardLayout>
+    <MainControls center>
+      <h1>Příprava zápasu</h1>
+      <p>
+        Na hlavní obrazovce je vidět QR kód s odkazem na zadání témat. Čekáme na
+        příchod sportovních komentátorů nebo konferenciéra.
+      </p>
+    </MainControls>
   </ControlsLayout>
 );
 
