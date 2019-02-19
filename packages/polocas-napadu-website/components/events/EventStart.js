1faad68dd49059b04507d953e07e34bffb9f4b35
Pavel Žák <pavel@berrycloud.cz>
2019-02-19 18:14:48 +0100

    Add event start component

---
+++ packages/polocas-napadu-website/components/events/EventStart.js
@@ -0,0 +1,38 @@
+import moment from 'moment';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import { withNamespaces } from '../../lib/i18n';
+
+const renderSingleDate = (date, format) => <span>{date.format(format)}</span>;
+
+export const EventStartInner = ({ allDay, end, start, t }) => {
+  if (!start) {
+    return t('event-start-indeterminate');
+  }
+  const startDate = moment(start);
+  if (!end) {
+    return renderSingleDate(startDate, allDay ? 'LL' : 'LLL');
+  }
+  const endDate = moment(end);
+  if (startDate.isSame(end, 'day')) {
+    if (allDay) {
+      return renderSingleDate(startDate, 'LL');
+    }
+    return (<span>{startDate.format('LLL')} - {endDate.format('LT')}</span>)
+  }
+  const format = allDay ? 'LL' : 'LLL';
+  return (<span>{startDate.format(format)} - {endDate.format(format)}</span>)
+};
+
+export const EventStart = withNamespaces(['events'])(EventStartInner);
+
+EventStart.propTypes = {
+  allDay: PropTypes.bool,
+  end: PropTypes.string,
+  start: PropTypes.string,
+};
+
+EventStart.defaultProps = {
+  allDay: false,
+};
---
+++ packages/polocas-napadu-website/components/events/__tests__/EventStart.js
@@ -0,0 +1,111 @@
+import moment from 'moment';
+import React from 'react';
+
+import { shallow } from 'enzyme';
+import { advanceTo, clear } from 'jest-date-mock';
+
+import { EventStartInner } from '..';
+import { translate } from '../../../mock';
+
+describe('EventStart component', () => {
+  const systemLocale = moment.locale();
+
+  beforeEach(() => {
+    advanceTo(new Date(2019, 1, 19, 8, 0, 0));
+    moment.locale('cs');
+  });
+
+  afterEach(() => {
+    clear();
+    moment.locale(systemLocale);
+  });
+
+  it('given no start date, renders start indeterminate message', () => {
+    const comp = shallow(<EventStartInner t={translate} />);
+    expect(comp).toIncludeText('event-start-indeterminate');
+  });
+
+  it.todo('given start datetime, renders fuzzy start datetime (in n hours)');
+
+  it('given start datetime, renders exact start datetime', () => {
+    const comp = shallow(
+      <EventStartInner
+        t={translate}
+        start="2019-02-19T09:00:00"
+      />
+    );
+    expect(comp).toIncludeText('19. únor 2019 9:00');
+  });
+
+  it.todo('given start all day date, renders fuzzy start date (in n days)');
+
+  it('given start all day date, renders exact start date', () => {
+    const comp = shallow(
+      <EventStartInner
+        allDay
+        start="2019-02-19T09:00:00"
+        t={translate}
+      />
+    );
+    expect(comp).toIncludeText('19. únor 2019');
+  });
+
+  it.todo('given start and end, renders fuzzy start datetime (in n hours)');
+
+  it('given start and end, renders exact start datetime', () => {
+    const comp = shallow(
+      <EventStartInner
+        start="2019-02-19T09:00:00"
+        end="2019-02-19T12:00:00"
+        t={translate}
+      />
+    );
+    expect(comp).toIncludeText('19. únor 2019 9:00');
+  });
+
+  it('given start and end on same day, renders end hours', () => {
+    const comp = shallow(
+      <EventStartInner
+        start="2019-02-19T09:00:00"
+        end="2019-02-19T12:00:00"
+        t={translate}
+      />
+    );
+    expect(comp).toIncludeText('19. únor 2019 9:00 - 12:00');
+  });
+
+  it('given allDay, start and end on same day, renders only events date', () => {
+    const comp = shallow(
+      <EventStartInner
+        allDay
+        start="2019-02-19T00:00:00"
+        end="2019-02-19T00:00:00"
+        t={translate}
+      />
+    );
+    expect(comp).toIncludeText('19. únor 2019');
+  });
+
+  it('given allDay, start and end on same day, renders events start and end date', () => {
+    const comp = shallow(
+      <EventStartInner
+        allDay
+        start="2019-02-19T00:00:00"
+        end="2019-03-20T00:00:00"
+        t={translate}
+      />
+    );
+    expect(comp).toIncludeText('19. únor 2019 - 20. březen 2019');
+  });
+
+  it('given start and end on same day, renders events start and end date time', () => {
+    const comp = shallow(
+      <EventStartInner
+        start="2019-02-19T10:00:00"
+        end="2019-03-20T15:00:00"
+        t={translate}
+      />
+    );
+    expect(comp).toIncludeText('19. únor 2019 10:00 - 20. březen 2019 15:00');
+  });
+});
