d9c28c978fe0174e007fca6f0d4d091d2e7d2e4e
Pavel Žák <pavel@berrycloud.cz>
2019-03-13 09:10:23 +0100

    Throw only on non-optional queries

--- packages/polocas-napadu-website/components/graphql/__tests__/withQuery.jsx
+++ packages/polocas-napadu-website/components/graphql/__tests__/withQuery.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import waait from 'waait';
+import React from 'react'
+import waait from 'waait'
 
-import { gql } from 'apollo-boost';
-import { shallow } from 'enzyme';
-import { MockedProvider } from 'react-apollo/test-utils';
+import { gql } from 'apollo-boost'
+import { mount, shallow } from 'enzyme'
+import { MockedProvider } from 'react-apollo/test-utils'
 
-import { withQuery } from '..';
+import { withQuery } from '..'
 
-const TestComponent = () => <span>Success</span>;
+const TestComponent = () => <span>Success</span>
 
 const TEST_QUERY = gql`
   query TestQuery {
@@ -15,9 +15,9 @@ const TEST_QUERY = gql`
       name
     }
   }
-`;
+`
 
-const TestComponentWrapped = withQuery(TestComponent, TEST_QUERY);
+const TestComponentWrapped = withQuery(TestComponent, TEST_QUERY)
 
 describe('withQuery HOC', () => {
   it('given query is loading, it renders loader', () => {
@@ -25,102 +25,102 @@ describe('withQuery HOC', () => {
       <MockedProvider mocks={[]}>
         <TestComponentWrapped />
       </MockedProvider>
-    ).dive().dive().dive();
-    expect(comp.find('QueryLoader')).toHaveLength(1);
-  });
+    ).dive().dive().dive()
+    expect(comp.find('QueryLoader')).toHaveLength(1)
+  })
 
   it('given query fails, it renders error component', async () => {
-    const error = new Error('Test!');
+    const error = new Error('Test!')
     const testQueryMock = {
       request: {
-        query: TEST_QUERY,
+        query: TEST_QUERY
       },
-      error,
-    };
+      error
+    }
     const comp = shallow(
       <MockedProvider mocks={[testQueryMock]} addTypename={false}>
         <TestComponentWrapped />
       </MockedProvider>
-    ).dive().dive().dive();
-    await waait(3);
-    expect(comp.find('QueryFailure')).toHaveLength(1);
-  });
+    ).dive().dive().dive()
+    await waait(3)
+    expect(comp.find('QueryFailure')).toHaveLength(1)
+  })
 
   it('given query succeeds, it renders annotated component', async () => {
     const testQueryMock = {
       request: {
-        query: TEST_QUERY,
+        query: TEST_QUERY
       },
       result: {
         data: {
           showList: [
             {
-              name: 'Poločas nápadu vs Kino',
-            },
-          ],
-        },
-      },
-    };
+              name: 'Poločas nápadu vs Kino'
+            }
+          ]
+        }
+      }
+    }
     const comp = shallow(
       <MockedProvider mocks={[testQueryMock]} addTypename={false}>
         <TestComponentWrapped />
       </MockedProvider>
-    ).dive().dive().dive();
-    await waait(3);
-    expect(comp.find('TestComponent')).toHaveLength(1);
-  });
+    ).dive().dive().dive()
+    await waait(3)
+    expect(comp.find('TestComponent')).toHaveLength(1)
+  })
 
   it('given query succeeds, it passes all props to annotated component', async () => {
     const testQueryMock = {
       request: {
-        query: TEST_QUERY,
+        query: TEST_QUERY
       },
       result: {
         data: {
           showList: [
             {
-              name: 'Poločas nápadu vs Kino',
-            },
-          ],
-        },
-      },
-    };
+              name: 'Poločas nápadu vs Kino'
+            }
+          ]
+        }
+      }
+    }
     const comp = shallow(
       <MockedProvider mocks={[testQueryMock]} addTypename={false}>
-        <TestComponentWrapped testProp="foo" />
+        <TestComponentWrapped testProp='foo' />
       </MockedProvider>
-    ).dive().dive().dive();
-    await waait(3);
-    expect(comp.find('TestComponent')).toHaveProp('testProp', 'foo');
-  });
+    ).dive().dive().dive()
+    await waait(3)
+    expect(comp.find('TestComponent')).toHaveProp('testProp', 'foo')
+  })
 
   it('given query succeeds, it passes data to annotated component', async () => {
     const testQueryMock = {
       request: {
-        query: TEST_QUERY,
+        query: TEST_QUERY
       },
       result: {
         data: {
           showList: [
             {
-              name: 'Poločas nápadu vs Kino',
-            },
-          ],
-        },
-      },
-    };
+              name: 'Poločas nápadu vs Kino'
+            }
+          ]
+        }
+      }
+    }
     const comp = shallow(
       <MockedProvider mocks={[testQueryMock]} addTypename={false}>
-        <TestComponentWrapped testProp="foo" />
+        <TestComponentWrapped testProp='foo' />
       </MockedProvider>
-    ).dive().dive().dive();
-    await waait(3);
+    ).dive().dive().dive()
+    await waait(3)
     expect(comp.find('TestComponent')).toHaveProp('data', {
       showList: [
         {
-          name: 'Poločas nápadu vs Kino',
-        },
-      ],
-    });
-  });
-});
+          name: 'Poločas nápadu vs Kino'
+        }
+      ]
+    })
+  })
+})
--- packages/polocas-napadu-website/components/graphql/withQuery.jsx
+++ packages/polocas-napadu-website/components/graphql/withQuery.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 import { Query } from 'react-apollo'
 
+import { QueryFailure } from './QueryFailure'
 import { QueryLoader } from './QueryLoader'
+
+function getEmptyKeys (data) {
+  return Object.keys(data).filter(key => !data[key])
+}
+
+function getNotFoundError (data) {
+  const empty = getEmptyKeys(data).join(', ')
+  const error = new Error(`Could not find ${empty}!`)
+  error.statusCode = 404
+  error.code = 'ENOENT'
+  return error
+}
 
 export const withQuery = (Component, query, optional = false) => (props) => (
   <Query query={query} variables={props.variables}>
@@ -11,13 +24,10 @@ export const withQuery = (Component, query, optional = false) => (props) => (
         return <QueryLoader />
       }
       if (error) {
-        throw error
+        return <QueryFailure error={error} />
       }
-      if (!optional && Object.keys(data).some(key => !data[key])) {
-        const error = new Error('Not Found!')
-        error.statusCode = 404
-        error.code = 'ENOENT'
-        throw error
+      if (!optional && getEmptyKeys(data).length > 0) {
+        throw getNotFoundError(data)
       }
       return <Component data={data} {...props} />
     }}
