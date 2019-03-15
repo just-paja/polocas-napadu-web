3ca37ac7eaaf094a1b62fb5fea81421729b6490f
Pavel Žák <pavel@berrycloud.cz>
2019-03-15 08:42:57 +0000

    Remove deprecation warnings

--- packages/polocas-napadu-website/components/layout/mui.js
+++ packages/polocas-napadu-website/components/layout/mui.js
@@ -2,6 +2,7 @@ import { createMuiTheme } from '@material-ui/core/styles'
 
 export const siteTheme = createMuiTheme({
   typography: {
+    useNextVariants: true,
     fontFamily: [
       'times' // @TODO: Use font from identity manual
     ]
