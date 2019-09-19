675934c7d69bfc4d367b612ce0c346b28f8f8f73
Pavel Žák <pavel@berrycloud.cz>
2019-09-19 20:40:45 +0200

    Render html language

---
+++ packages/polocas-napadu-website/pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+import { lngFromReq } from 'next-i18next/dist/commonjs/utils'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    const lng = lngFromReq(ctx.req)
+    return {  ...initialProps, lng }
+  }
+
+  render() {
+    const { lng } = this.props
+    return (
+      <html lang={lng}>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
