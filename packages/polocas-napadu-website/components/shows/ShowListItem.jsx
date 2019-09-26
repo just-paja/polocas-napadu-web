611f28d47aa26fa9fb6ff28a03700c3030c989cb
Pavel Žák <pavel@berrycloud.cz>
2019-09-26 09:30:03 +0200

    Link to show detail trough the whole item

--- packages/polocas-napadu-website/components/shows/ShowListItem.jsx
+++ packages/polocas-napadu-website/components/shows/ShowListItem.jsx
@@ -6,29 +6,35 @@ import { Link } from '../../routes'
 import { Show } from '../proptypes'
 import { EventLocation, EventStart } from '../events'
 
-export const ShowListItem = ({ show }) => (
-  <div className={styles.host}>
-    <div className={classnames('lead', styles.item)}>
-      <EventStart
-        allDay={show.allDay}
-        className={styles.date}
-        end={show.end}
-        start={show.start}
-      />
-      {' '}
-      <Link route='showDetail' params={{ slug: show.slug }}>
-        <a>{show.name}</a>
-      </Link>
-    </div>
-    <div className={styles.item}>
-      <span className={styles.date}>
-        {show.location && <EventLocation location={show.location} />}
-      </span>
-      {' '}
-      {show.showType && show.showType.name}
-    </div>
-  </div>
-)
+export function ShowListItem ({ show }) {
+  const isFuture = false
+  return (
+    <Link
+      route='showDetail'
+      params={{ slug: show.slug }}
+    >
+      <a className={classnames(styles.host, { [styles.future]: isFuture })}>
+        <div className={classnames('lead', styles.item, styles.topLine)}>
+          <EventStart
+            allDay={show.allDay}
+            className={styles.date}
+            end={show.end}
+            start={show.start}
+          />
+          {' '}
+          <span>{show.name}</span>
+        </div>
+        <div className={styles.item}>
+          <span className={styles.date}>
+            {show.location && <EventLocation location={show.location} />}
+          </span>
+          {' '}
+          {show.showType && show.showType.name}
+        </div>
+      </a>
+    </Link>
+  )
+}
 
 ShowListItem.propTypes = {
   show: Show.isRequired
