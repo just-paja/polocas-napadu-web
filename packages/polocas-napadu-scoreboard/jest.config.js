a93bce2d16846b12dfa7300a82f988f4da8d79b6
just-paja <just.paja@gmail.com>
2019-12-09 10:51:17 +0100

    WIP

--- packages/polocas-napadu-scoreboard/jest.config.js
+++ packages/polocas-napadu-scoreboard/jest.config.js
@@ -1,22 +1,25 @@
-const base = require('./jest.config.base.js')
+// const base = require('./jest.config.base.js')
 
 module.exports = {
-  ...base,
+  // ...base,
+  // roots: ['<rootDir>/packages/scoreboard'],
+  // roots: ['<rootDir>/src'],
   projects: [
-    '<rootDir>/packages/*/jest.config.js',
-    {
-      displayName: 'linter',
-      runner: 'jest-runner-standard',
-      testMatch: [
-        '<rootDir>/**/*.{js,jsx}'
-      ],
-      testPathIgnorePatterns: [
-        '/build/',
-        '/coverage/',
-        '/node_modules/',
-        '/static/'
-      ]
-    }
+    // '<rootDir>/packages/*/jest.config.js',
+    '<rootDir>/packages/scoreboard/jest.config.js',
+    // {
+    //   displayName: 'linter',
+    //   runner: 'jest-runner-standard',
+    //   testMatch: [
+    //     '<rootDir>/**/*.{js,jsx}'
+    //   ],
+    //   testPathIgnorePatterns: [
+    //     '/build/',
+    //     '/coverage/',
+    //     '/node_modules/',
+    //     '/static/'
+    //   ]
+    // }
   ],
-  coverageDirectory: '<rootDir>/coverage/'
+  // coverageDirectory: '<rootDir>/coverage/'
 }
--- packages/polocas-napadu-scoreboard/packages/scoreboard/jest.standard.config.js
+++ packages/polocas-napadu-scoreboard/packages/scoreboard/jest.standard.config.js
@@ -2,9 +2,7 @@ const base = require("../../jest.config.base.js");
 const pack = require('./package');
 
 module.exports = {
-  // displayName: 'Scoreboard',
-  // name: pack.name,
-  displayName: 'linter',
+  displayName: `${pack.name} linter`,
   runner: 'jest-runner-standard',
   testMatch: [
     '<rootDir>/**/*.{js,jsx}'
