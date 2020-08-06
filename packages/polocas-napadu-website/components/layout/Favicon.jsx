9781bb421d706de18afb8af31d31d97ab3a91e97
just-paja <pavel@zak.global>
2020-08-06 10:52:44 +0200

    Point to static dir

--- packages/polocas-napadu-website/components/layout/Favicon.jsx
+++ packages/polocas-napadu-website/components/layout/Favicon.jsx
@@ -6,24 +6,24 @@ export const Favicon = () => (
     <link
       rel='apple-touch-icon'
       sizes='180x180'
-      href='/public/static/favicon/apple-touch-icon.png'
+      href='/static/favicon/apple-touch-icon.png'
     />
     <link
       rel='icon'
       type='image/png'
       sizes='32x32'
-      href='/public/static/favicon/favicon-32x32.png'
+      href='/static/favicon/favicon-32x32.png'
     />
     <link
       rel='icon'
       type='image/png'
       sizes='16x16'
-      href='/public/static/favicon/favicon-16x16.png'
+      href='/static/favicon/favicon-16x16.png'
     />
-    <link rel='manifest' href='/public/static/favicon/site.webmanifest' />
+    <link rel='manifest' href='/static/favicon/site.webmanifest' />
     <link
       rel='mask-icon'
-      href='/public/static/favicon/safari-pinned-tab.svg'
+      href='/static/favicon/safari-pinned-tab.svg'
       color='#007120'
     />
     <meta name='msapplication-TileColor' content='#007120' />
