76d573262fdde7ab0375a65c6b5fce47d490a3b6
just-paja <pavel@zak.global>
2020-08-25 00:10:54 +0200

    Use custom server.js

--- packages/longforman-website/server.js
+++ packages/longforman-website/server.js
@@ -1,21 +1,14 @@
 const express = require('express')
 const next = require('next')
-const routes = require('./routes')
-
-const nextI18next = require('./lib/i18n')
-const middleware = require('./lib/i18n-middleware')
 
 const app = next({ dev: process.env.NODE_ENV !== 'production' })
-const handle = routes.getRequestHandler(app)
+const handle = app.getRequestHandler()
 const port = process.env.NODE_PORT || 3000
 
 async function main () {
   await app.prepare()
   const server = express()
-
-  server.use(middleware(nextI18next))
   server.use(handle)
-
   await server.listen(port)
   console.log(`>>>> Ready on http://localhost:${port}`)
 }
