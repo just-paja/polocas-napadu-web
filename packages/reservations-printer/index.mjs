b9e334e8d320482336e30197c591887550ee56e1
Pavel Žák <pavel@zak.global>
2022-02-23 00:20:18 +0100

    feat: add reservation printer helper

---
+++ packages/reservations-printer/index.mjs
@@ -0,0 +1,145 @@
+import fetch from 'cross-fetch'
+import open from 'open'
+import pdf from 'html-pdf'
+
+import { file } from 'tmp-promise'
+import { parse } from 'csv-parse'
+
+const getCsvUrl = (documentId, sheetId) => 
+  `https://docs.google.com/spreadsheets/d/${documentId}/export?format=csv&gid=${sheetId}`
+
+const getCsvStream = async url => {
+  const res = await fetch(url)
+  if (res.ok && res.status >= 200 && res.status < 300) {
+    return res.body
+  }
+
+  throw new Error(`Failed to initiate CSV stream ${res.status}`)
+}
+
+const parseDocumentId = absUrl => {
+  const url = new URL(absUrl)
+  const documentId = url.pathname.split('/')[3]
+  const [,sheetId] = url.hash.substring(1).split('&').map(item => item.split('=')).find(([key]) => key === 'gid')
+  return {documentId, sheetId}
+}
+
+const readSheet = async src => await new Promise((resolve ,reject) => {
+  const records = []
+  const parser = parse()
+  parser.on('readable', () => {
+    let record 
+    do {
+      record = parser.read()
+      if (record) {
+        records.push(record)
+      }
+    } while(record)
+  })
+  parser.on('error', reject)
+  parser.on('end', () => resolve(records))
+  src.pipe(parser)
+})
+
+const name = 'Vaše jméno'
+const count = 'Počet rezervací'
+const locationSpec = 'Kde chcete sedět?'
+
+const parseValue = (header, value) => {
+  if (header === count) {
+    return parseInt(value, 10)
+  }
+  return value
+}
+
+const parseRecords = records => {
+  const headers = records.shift()
+  return records.map(record => record.reduce(
+    (aggr, value, column) => Object.assign(aggr, {
+      [headers[column]]: parseValue(headers[column], value),
+    }),
+    {})
+  )
+}
+
+const duplicateReservation = reservation => Array(reservation[count]).fill(reservation, 0, reservation[count])
+
+const addIndexes = (res, index, src) => ({
+  ...res,
+  ordinal: index + 1,
+  total: src.length,
+})
+
+const mapDataToDoc = data => data
+  .reduce((aggr, reservation) => [...aggr, ...(duplicateReservation(reservation).map(addIndexes))], [])
+  .map(reservation => `
+    <div class="person">
+      <div class="text">
+        <div>${reservation[name]}</div>
+        <div>
+          <span class="locationSpec">(${reservation[locationSpec]})</span><br />
+          <span class="numbers">(${reservation.ordinal}/${reservation.total})</span>
+        </div>
+      </div>
+    </div>`)
+  .join('\n')
+
+const style = `
+  html, body {
+    font-family: 'HK Blocker';
+    font-size: 1.75rem;
+    height: 100%;
+    margin: 0 auto;
+    text-transform: uppercase;
+    width: 98%;
+  }
+  .person {
+    display: table;
+    height: 50%;
+    width: 100%;
+  }
+  .text {
+    display: table-cell;
+    text-align: center;
+    vertical-align: middle;
+  }
+  .locationSpec, .numbers {
+    color: #999;
+    font-size: 1rem;
+  }
+`
+
+const createDocument = data => {
+  return `<!DOCTYPE html><html><head><style>${style}</style></head><body>${mapDataToDoc(data)}</body></html>`
+}
+
+const createPdf = async (doc, dest) => await new Promise((resolve, reject) => { 
+  const options = {
+    border: '10mm',
+    format: 'A4',
+    orientation: 'portrait',
+  }
+  pdf.create(doc, options).toFile(dest, (err, res) => {
+    if (err) {
+      reject(err)
+    } else {
+      resolve(res)
+    }
+  })
+})
+
+const main = async (documentUrl) => {
+  const {documentId, sheetId} = parseDocumentId(documentUrl)
+  const url = getCsvUrl(documentId, sheetId)
+  const src = await getCsvStream(url)
+  const records = await readSheet(src)
+  const data = parseRecords(records)
+  const dest = await file({ postfix: 'print.pdf' })
+  const html = createDocument(data)
+  await createPdf(html, dest.path)
+  await open(dest.path, { wait: true })
+  await dest.cleanup()
+}
+
+await main(process.argv[2])
+
