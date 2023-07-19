7c1f10f8d14fc0a38de0ff40b10bd2c0b43b01a6
Pavel Žák <pavel@zak.global>
2023-07-19 15:48:27 +0200

    feat: pass GTM code

--- packages/website/next.config.js
+++ packages/website/next.config.js
@@ -3,7 +3,8 @@ const path = require('path')
 module.exports = {
   publicRuntimeConfig: {
     API_URL: process.env.NODE_API_URL || 'http://localhost:8000/graphql',
-    GA_CODE: process.env.NODE_GA_CODE || null
+    GA_CODE: process.env.NODE_GA_CODE || null,
+    GTM_CODE: process.env.NODE_GTM_CODE || null
   },
   sassOptions: {
     includePaths: [
